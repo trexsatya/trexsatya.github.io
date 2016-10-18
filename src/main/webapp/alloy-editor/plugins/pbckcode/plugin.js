ae120fda56d535af7fb1e074db1c6d82b4ef3a2d
trexsatya <kumarsatya1990@gmail.com>
2016-10-18 21:58:51 +0530

    refactoring to modules and dummy authentication added

--- src/main/webapp/alloy-editor/plugins/pbckcode/plugin.js
+++ src/main/webapp/alloy-editor/plugins/pbckcode/plugin.js
@@ -35,7 +35,7 @@ CKEDITOR.plugins.add('pbckcode', {
             ],
             theme    : 'textmate',
             tab_size : 4,
-            js       : "/static//js//ext-dependencies///"
+            js       : "http://cdn.jsdelivr.net//ace/1.1.4/noconflict///"
         };
 
         // merge user settings with default settings
