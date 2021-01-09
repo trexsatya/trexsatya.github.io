497988b7dd317df3c9d3df25537401cb44dbebcd
Satyendra Kumar <kumarsatya1990@gmail.com>
2021-01-09 23:56:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-API_URL = `http://satyendra.online:8080/api/`
+API_URL = `https://trexsatya.github.io/db/`
 imageCdnUrl = 'https://storage.googleapis.com/cupitor-220103.appspot.com'; //`http://${location.hostname}:8080/images/`
