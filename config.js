972d6cb870a51ae2b6eb729c995ccf9447aaf428
Satya <kumarsatya1990@gmail.com>
2021-01-10 00:04:18 +0530

    Updates

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-API_URL = `https://trexsatya.github.io/db/`
+API_URL = `https://raw.githubusercontent.com/trexsatya/trexsatya.github.io/gh-pages/db/`
 imageCdnUrl = 'https://storage.googleapis.com/cupitor-220103.appspot.com'; //`http://${location.hostname}:8080/images/`
