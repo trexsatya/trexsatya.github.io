e450e39e050cb0023a189268e1adc77e81e85477
Satyendra Kumar <satyendra.kumar@klarna.com>
2022-10-19 17:37:44 +0200

    Update DB

--- prepare.py
+++ prepare.py
@@ -10,9 +10,9 @@ def prepare_search_data():
                 with open(p) as f:
                     d = json.load(f)
                     if d and d['name']:
-                        names.append({"name": d['name'], "id": d['id']})
+                        names.append({"name": d['name'], "id": d.get('id', str(file))})
             except Exception as e:
-                print("exception", file, d)
+                print("Error", e, file, d)
 
     with open("db/search", "w") as f:
         f.write(json.dumps(names))	        		
@@ -22,10 +22,10 @@ def prepare_search_data():
 def prepare_articles_data():
     subjects = {}
 
-    def add_to_subject(subject, d):
+    def add_to_subject(subject, d, file):
         items = subjects[subject] if subject in subjects else []
         items.append({
-               "name": d['name'], "id": d['id'], "img": d.get("img"),
+               "name": d['name'], "id": d.get('id', str(file)), "img": d.get("img"),
                "subject": d.get("subject"),
                "summary": d.get("summary"), "lastUpdated": d.get("lastUpdated"),
                "tags": d.get("tags") if d.get("tags") else [],
@@ -39,10 +39,10 @@ def prepare_articles_data():
                 p = os.path.join(subdir, file)
                 with open(p) as f:
                     d = json.load(f)
-                    if d and d['subject']:
-                        add_to_subject(d['subject'], d)
+                    if d and 'subject' in d:
+                        add_to_subject(d['subject'], d, file)
             except Exception as e:
-                print("exception", file, d)
+                print("exception", e, file, d)
 
     for subject, items in subjects.items():
         with open(f"db/articles/{subject}", "w") as f:
