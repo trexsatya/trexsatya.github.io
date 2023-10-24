f3dfd5ee5e903912b47e255c95625ff19ebe41da
Satyendra Kumar <kumarsatya1990@gmail.com>
2023-10-24 20:45:15 +0200

    Update data-structures.js

--- data-structures.js
+++ data-structures.js
@@ -411,32 +411,4 @@ function combinations(collection, n) {
   return recur(array, n);
 }
 
-const stringSimilarity = function (str1, str2, substringLength, caseSensitive) {
-  if (substringLength === void 0) {
-    substringLength = 2;
-  }
-  if (caseSensitive === void 0) {
-    caseSensitive = false;
-  }
-  if (!caseSensitive) {
-    str1 = str1.toLowerCase();
-    str2 = str2.toLowerCase();
-  }
-  if (str1.length < substringLength || str2.length < substringLength)
-    return 0;
-  const map = new Map();
-  for (let i = 0; i < str1.length - (substringLength - 1); i++) {
-    const substr1 = str1.substr(i, substringLength);
-    map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
-  }
-  let match = 0;
-  for (let j = 0; j < str2.length - (substringLength - 1); j++) {
-    const substr2 = str2.substr(j, substringLength);
-    const count = map.has(substr2) ? map.get(substr2) : 0;
-    if (count > 0) {
-      map.set(substr2, count - 1);
-      match++;
-    }
-  }
-  return (match * 2) / (str1.length + str2.length - ((substringLength - 1) * 2));
-};
+
