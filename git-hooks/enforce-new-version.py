329929fc565730e462cb524b56634a4e63c692f6
Satyendra Kumar <satyendra.kumar@klarna.com>
2024-09-15 19:15:29 +0200

    Verify that version is not overridden

--- git-hooks/enforce-new-version.py
+++ git-hooks/enforce-new-version.py
@@ -1,30 +1,24 @@
 import subprocess
 
 VOCAB_VERSION_TXT = "git-hooks/vocab-version.txt"
+VOCAB_FILE = "db/language/swedish/vocabulary.txt"
 
-def get_existing_version():
-    with open(VOCAB_VERSION_TXT) as f:
-        return f.readline().strip()
+def get_modified_lines():
+    p = subprocess.Popen(f"git diff --unified=0 {VOCAB_FILE}".split(), stdout=subprocess.PIPE)
+    return "\n".join(list(map(lambda x: x.decode("utf-8").strip(), p.stdout.readlines())))
 
-def save_new_version(version):
-    with open(VOCAB_VERSION_TXT, "w") as f:
-        f.write(version)
-
-def commit_new_version(version):
-    p = subprocess.Popen(f"git add {VOCAB_VERSION_TXT} && git commit -m 'Update vocabulary version to {version}' {VOCAB_VERSION_TXT}".split(),
-                     stdout=subprocess.PIPE)
-    retval = p.wait()
-    if retval != 0:
-        raise Exception("Failed to commit new version")
+def get_original_version():
+    p = subprocess.Popen(f"git show  HEAD:{VOCAB_FILE}".split(), stdout=subprocess.PIPE)
+    first_line = p.stdout.readlines()[0].decode("utf-8").strip()
+    return int(first_line.split("=")[1])
 
 def ensure_new_version(path):
     with open(path) as f:
         first_line = f.readline().strip()
         if first_line.startswith("@version="):
-            version = first_line.split("=")[1]
-            if version <= get_existing_version():
+            version = int(first_line.split("=")[1])
+            if version <= get_original_version():
                 raise Exception(f"You must update the version in {path}")
-            save_new_version(version)
             print(f"New version found: {version}")
 
 print("Enforcing new version")
